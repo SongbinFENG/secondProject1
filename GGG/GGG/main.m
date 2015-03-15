be96907ee93dccf176abe3ccb452af6c3f18598e
SongbinFENG <adam.songbin@gmail.com>
2015-03-15 22:45:35 +0100

    1.1

---
+++ GGG/GGG/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GGG
+//
+//  Created by 冯松彬 on 15/3/15.
+//  Copyright (c) 2015年 冯松彬. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
+fsb
